6c775227c3a2c86eca4e941b633f3f72868ad2c0
Leonardo Utreras <ljutreras96@gmail.com>
2022-08-30 21:54:09 -0400

    Creacion de FETCH JS

--- Proyecto/assets/script.js
+++ Proyecto/assets/script.js
@@ -1,13 +1,28 @@
 window.addEventListener('DOMContentLoaded',(e)=>{
     const boton = document.querySelector("#contacto");
-    boton.addEventListener('click', mostrarFormulario);
+    boton.addEventListener('click', (ev)=>{
+        //TODO crear funcionalidad
 
-    function mostrarFormulario (){
-        //TODO crear funcion de mostrar formulario
-    }
+
+
+
+
+    });
+
+    
 
 
 
 
 
 })
+
+async function guardarUsuario(suscriptor){
+
+    const url = "https://proyectoaiep-842fb-default-rtdb.firebaseio.com/registro.json";
+    const respuesta = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(suscriptor)
+    });
+    const data = await respuesta.json();
+}
